ce6314c95ffc4efef5c555475530d9e2857ab2a6
Chase <chasesmith@park6.org>
2025-04-28 20:04:54 -0600

    put in passcode verification for arithmeticCalculator, int main, and deleted random file x, added codes directory, codes.txt

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,73 +1,161 @@
 #include <iostream> 
 #include <cmath>
+
+double arithmeticCalculator();
+std::string algebraCalculator();
+void passcodeVerifier();
+
+int main(){
+
+    passcodeVer`ifier();
+     
+    std::string code;
  
-void arithmeticCalculator(){
-     double num1;
-     double num2;
-     double result;
-     char op;
+    std::cout << "****** Terminal ******\n";
  
+    std::cout << "Enter the code for your chosen destination: ";
+    std::cin >> code;
  
-     std::cout << "****** Calculator ******";
-     std::cout << "Arithemetic Calculator\n";
+    if(code == 1234){
+        arithmeticCalculator();
+    }
+    else if(code == 123xyz){
+        algebraCalculator();
+    }
+    else{
+        std::cout << "Enter an approved code\n"
+    }
  
-     std::cout << "Enter #1: ";
-     std::cin >> num1;
- 
-     std::cout << "Enter operator(+ - * /): ";
-     std::cin >> op;
- 
-     std::cout << "Enter #2: ";
-     std::cin >> num2;
- 
-     switch(){
-         case '+':
-             result = num1 + num2;
-             std::cout << result;
-             break;
-         case '-':
-             result = num1 - num2;
-             std::cout << result;
-             break;
-         case '*':
-             result = num1 * num2;
-             std::cout << result;
-             break;
-         case '/':
-         default:
-             std::cout << "Enter a valid response"
-     }
-     std::cout << "************************"
- }
- 
-void algebraCalculator(){
-    int 
+    std::cout << "*********************************";
+     
+    return 0;
 }
 
+double arithmeticCalculator(){
+    double num1;
+    double num2;
+    double result;
+    std::string enteredCode;
+
+    do{
+        std::cout << "Enter code: ";
+        std::cin >> enteredCode;
+    }
+    while(enteredCode != "1+1" || enteredCode != "1-1" || enteredCode != "1*1" || enteredCode != "1/1")
+
+    std::cout << "Enter #1: ";
+    std::cin >> num1;
+    
+    std::cout << "Enter #2: ";
+    std::cin >> num2;
+
+    switch(enteredCode){
+        case '1+1':
+            result = num1 + num2;
+            std::cout << result;
+            break;
+        case '1-1':
+            result = num1 - num2;
+            std::cout << result;
+            break;
+        case '1*1':
+            result = num1 * num2;
+            std::cout << result;
+            break;
+        case '1/1':
+            result = num1 / num2;
+            std::cout << result;
+            break;
+        default:
+            std::cout << "Enter a valid response";
+    }
+
+    std::cout << "************************";
+
+    return result;
+}
+
+std::string algebraCalculator(){
+    std::string equation;
+    std::string result;
+    int varNum;
+    std::string var1;
+    std::string var2;
+    std::string var3;
+    std::string var4;
+    std::string var5;
+    std::string var6;
 
 
- int main(){
- 
-     calculator();
-     
-     std::string calcType;
- 
-     std::cout << "****** Calculator Terminal ******\n";
- 
-     std::cout << "Enter calculator type (arithmetic, algebra): ";
-     std::cin >> calcType;
- 
-     if(calcType = arithmetic){
-         arithmeticCalculator();
-     }
-     else if(calcType = algebra){
-         std::cout << "Still developing this feature\n"
-     }
-     else{
-         std::cout << "Enter a valid response\n"
-     }
- 
-     std::cout << "*********************************";
-     
-     return 0;
- }
+    std::cout << "Number of variables(Max: 6): ";
+    std::cin >> varNum;
+
+    std::cout << "Enter equation: ";
+    std::cin >> equation;
+
+    std::cout << "Enter coefficient with variable\n";
+
+    switch(varNum){
+        case 1:
+            std::cout << "Enter your variable: ";
+            std::cin >> var1;
+        case 2:
+            std::cout << "Enter variable 1: ";
+            std::cin >> var1;
+            std::cout << "Enter variable 2: ";
+            std::cin >> var2
+        case 3:
+            std::cout << "Enter variable 1: ";
+            std::cin >> var1;
+            std::cout << "Enter variable 2: ";
+            std::cin >> var2;
+            std::cout << "Enter variable 3: ";
+            std::cin >> var3;
+        case 4:
+            std::cout << "Enter variable 1: ";
+            std::cin >> var1;
+            std::cout << "Enter variable 2: ";
+            std::cin >> var2;
+            std::cout << "Enter variable 3: ";
+            std::cin >> var3;
+            std::cout << "Enter variable 4: ";
+            std::cin >> var4;
+        case 5:
+            std::cout << "Enter variable 1: ";
+            std::cin >> var1;
+            std::cout << "Enter variable 2: ";
+            std::cin >> var2;
+            std::cout << "Enter variable 3: ";
+            std::cin >> var3;
+            std::cout << "Enter variable 4: ";
+            std::cin >> var4;
+            std::cout << "Enter variable 5: ";
+            std::cin >> var5;
+        case 6:
+            std::cout << "Enter variable 1: ";
+            std::cin >> var1;
+            std::cout << "Enter variable 2: ";
+            std::cin >> var2;
+            std::cout << "Enter variable 3: ";
+            std::cin >> var3;
+            std::cout << "Enter variable 4: ";
+            std::cin >> var4;
+            std::cout << "Enter variable 5: ";
+            std::cin >> var5;
+            std::cout << "Enter variable 6: ";
+            std::cin >> var6;
+    }
+
+    return result;
+}
+
+void passcodeVerifier(){
+    std::string passcode = skibidi rizz;
+    std::string enteredPasscode; 
+
+    do{
+        std::cout << "Enter passcode: ";
+        std::cin >> enteredPasscode;
+    }
+    while(enteredPasscode != passcode)
+} 
