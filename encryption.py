85e9a503797d0907340f629c92cb760c02b21738
Chase <chasesmith@park6.org>
2025-09-17 15:07:58 -0600

    finished encryption.py

--- encryption.py
+++ encryption.py
@@ -1,20 +1,48 @@
 import random
 import string
 
-chars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]d
-def encrypt():
+chars = list(string.ascii_letters + string.digits + string.punctuation + " ")
+key = chars.copy()
+random.shuffle(key)  # Key is a shuffled version of chars
+
+def encrypt(plain_text):
+    cipher_text = []
+    for char in key:
+        print(char, end="")
+    print()
     for char in plain_text:
-        index = chars.index(char)
-        cipher_text.append(key.index(index))
-    print(cipher_text)
+        if char in chars:
+            idx = chars.index(char)
+            cipher_text.append(key[idx])
+        else:
+            cipher_text.append(char)
+            
+    return ''.join(cipher_text)
 
-
-print("------ Encryption Terminal ------")
-print("Enter your choice: ")
-print("1 - Encrypt")
-print("2 - Decrypt")
-choice = input("Enter here: ")
-if choice == "1":
-    plain_text = input("Enter text: ")
-    encrypt()
+def decrypt(cipher_text):
+    plain_text = []
+    for char in cipher_text:
+        if char in key:
+            idx = key.index(char)
+            plain_text.append(chars[idx])
+        else:
+            plain_text.append(char)
+    return ''.join(plain_text)
     
+while True:
+    print("------ Encryption Terminal ------")
+    print("Enter your choice: ")
+    print("1 - Encrypt")
+    print("2 - Decrypt")
+    print("3 - Quit")
+    choice = input("Enter here: ")
+    
+    if choice == "1":
+        plain_text = input("Enter text: ")
+        print("Encrypted:", encrypt(plain_text))
+    elif choice == "2":
+        cipher_text = input("Enter cipher text: ")
+        key = input("Enter key: ")
+        print("Decrypted:", decrypt(cipher_text))
+    elif choice == "3":
+        break;
